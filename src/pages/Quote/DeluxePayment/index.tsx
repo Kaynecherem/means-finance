f7275f12cad760717d457e638e9f671daecacb48
Necherem <kaluchinecherem8@gmail.com>
2025-06-13 23:53:57 +0200

    Fix DeluxePayment embed using iframe

--- src/pages/Quote/DeluxePayment/index.tsx
+++ src/pages/Quote/DeluxePayment/index.tsx
@@ -1,5 +1,5 @@
 import { Col, Row } from 'antd';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import SubmitButton from '../../../components/Form/SubmitButton';
@@ -12,37 +12,68 @@ const DeluxePayment: React.FC = () => {
     const deluxeToken = useSelector(({ auth }: RootState) => (auth.agency as any)?.deluxe_partner_token);
 
     useEffect(() => {
-        if (showEmbed) {
-            const script = document.createElement('script');
-            script.src = 'https://hostedpaymentform.deluxe.com/v2/deluxe.js';
-            script.onload = () => {
-                const HostedForm = (window as any).HostedForm;
-                if (HostedForm) {
-                    const options = {
-                        containerId: 'deluxe-container',
-                        xtoken: deluxeToken,
-                        xrtype: 'Create Vault',
-                        xpm: '0',
-                        xdisplayvafields: 'true',
-                        xcssid: 'mycustomcss'
-                    };
-                    HostedForm.init(options, {
-                        onSuccess: (data: unknown) => {
-                            console.log(JSON.stringify(data));
-                            alert('Payment method added successfully');
-                            navigate('/agency/quote/customer-info');
-                        },
-                        onFailure: (data: unknown) => { console.log(JSON.stringify(data)); },
-                        onInvalid: (data: unknown) => { console.log(JSON.stringify(data)); }
-                    }).then((instance: any) => instance.renderHpf());
-                }
-            };
-            document.body.appendChild(script);
-            return () => {
-                document.body.removeChild(script);
-            };
-        }
-    }, [showEmbed, deluxeToken, navigate]);
+        const handleMessage = (e: MessageEvent) => {
+            if (e.data === 'deluxe_success') {
+                alert('Payment method added successfully');
+                navigate('/agency/quote/customer-info');
+            }
+        };
+        window.addEventListener('message', handleMessage);
+        return () => window.removeEventListener('message', handleMessage);
+    }, [navigate]);
+
+    const iframeDoc = useMemo(() => {
+        return `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <script src="https://hostedpaymentform.deluxe.com/v2/deluxe.js"></script>
+  <style id="mycustomcss">
+    body {
+      padding: 24px;
+      border-radius: 25px;
+      height: 100%;
+    }
+    .form-control {
+      color: green;
+      border: 1px solid #0d6efd;
+    }
+    .form-label {
+      color: black;
+    }
+    .btn {
+      color: green;
+      border: 1px solid green;
+    }
+    /* In the iframe's CSS */
+    .iframe-wrapper #dppPaymentContainer {
+      height: 100% !important;
+    }
+  </style>
+</head>
+<body>
+<div id="mycontainer"></div>
+<script>
+  var options = {
+    containerId: "mycontainer",
+    xtoken: "${deluxeToken}",
+    xrtype: "Create Vault",
+    xpm: "0",
+    xdisplayvafields: "true",
+    xcssid: "mycustomcss"
+  };
+
+  HostedForm.init(options, {
+    onSuccess: function(data) {
+      console.log(JSON.stringify(data));
+      window.parent.postMessage('deluxe_success', '*');
+    },
+    onFailure: function(data) { console.log(JSON.stringify(data)); },
+    onInvalid: function(data) { console.log(JSON.stringify(data)); }
+  }).then(function(instance) { instance.renderHpf(); });
+</script>
+</body>
+</html>`;
+    }, [deluxeToken]);
 
     return (
         <Row gutter={[0, 20]} justify={"center"}>
@@ -57,7 +88,13 @@ const DeluxePayment: React.FC = () => {
                 )}
             </Col>
             <Col span={24}>
-                {showEmbed && <div id="deluxe-container" style={{ width: '100%' }} />}
+                {showEmbed && (
+                    <iframe
+                        title="Deluxe Payment"
+                        srcDoc={iframeDoc}
+                        style={{ width: '100%', border: 'none', height: '700px' }}
+                    />
+                )}
             </Col>
         </Row>
     );
