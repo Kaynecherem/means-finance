c3c3aab6017a895d49c3b040792b8728e9986f53
Necherem <kaluchinecherem8@gmail.com>
2025-08-18 03:12:48 +0200

    feat: open deluxe modal for new payment methods

--- src/pages/Collect/Payment/CardPayment.tsx
+++ src/pages/Collect/Payment/CardPayment.tsx
@@ -1,14 +1,8 @@
 import { Col, Row, Switch } from 'antd';
 import FeesInfoCard from '../../../components/FeesInfoCard';
-import CardNumberInput from '../../../components/Form/CardNumberInput';
-import CVVInput from '../../../components/Form/CVVInput';
-import ExpiryDateInput from '../../../components/Form/ExpiryDateInput';
-import FormItem from '../../../components/Form/FormItem';
+import SubmitButton from '../../../components/Form/SubmitButton';
 import UserCards from '../../../components/UserCards';
 import { Card, PaymentRecordingWith } from '../../../utils/types/common';
-import cardNumberValidator from '../../../utils/validators/cardNumberValidator';
-import expiryDateValidator from '../../../utils/validators/expiryDateValidator';
-import { PageSubHeader } from '../../style';
 import { ChangeAccountInfoText } from './style';
 const CardPayment = (props: {
     autoPayment: boolean,
@@ -17,53 +11,15 @@ const CardPayment = (props: {
     cards?: Card[],
     onCardSelect?: (cardId: string) => void
     paymentRecordingWith: PaymentRecordingWith
-    amount?: number
+    amount?: number,
+    onAddCard?: () => void
 }) => {
     return <Row justify={'center'} gutter={[12, 24]}>
         <Col span={24}>
             <UserCards loading={props.loading} cards={props.cards} agencyId={null} selectMode onSelect={props.onCardSelect} paymentRecordingWith={props.paymentRecordingWith} />
         </Col>
-        <Col>
-            <PageSubHeader>OR</PageSubHeader>
-        </Col>
-        <Col span={24}>
-            <FormItem
-                label="Card Number"
-                name="cardNumber"
-                rules={[
-                    {
-                        required: true
-                    },
-                    {
-                        validator: cardNumberValidator
-                    }
-                ]}
-            >
-                <CardNumberInput />
-            </FormItem>
-        </Col>
-        <Col span={12}>
-            <FormItem
-                label="Exp"
-                name="expiry"
-                rules={[
-                    {
-                        validator: expiryDateValidator
-                    }
-                ]}
-            >
-                <ExpiryDateInput />
-            </FormItem>
-        </Col>
-        <Col span={12}>
-            <FormItem
-                label="CVV"
-                name="cvv"
-                rules={[{ required: true }]}
-
-            >
-                <CVVInput />
-            </FormItem>
+        <Col span={24} style={{ textAlign: 'center' }}>
+            <SubmitButton htmlType="button" onClick={props.onAddCard}>Add New Card</SubmitButton>
         </Col>
         <Col>
             <ChangeAccountInfoText>
